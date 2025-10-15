873198333e4900bbb67d00b281a0bb3b7a6744a8
arciterobert123-afk <arciterobert123@gmail.com>
2025-10-15 00:24:56 -0700

    "Uploaded C case problem and outpot screenshot."

---
+++ main.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+int main() {
+     printf("Name: Robert I. Arcite Jr.");
+      printf("ID Number: 2025306851\n");
+
+      int num1 = 15, num2 = 25, sum;
+      sum = num1 + num2;
+
+      printf("Result: The sum of %d and %d is %d\n", num1, num2, sum);
+
+      return 0;
+}
+      
